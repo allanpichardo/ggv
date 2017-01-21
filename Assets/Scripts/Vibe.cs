574f00c20cdaa217802d41a3781c1f00796f59c2
Allan Pichardo <allanpichardo@Allans-MacBook-Pro.local>
2017-01-21 16:49:14 -0500

    removed merge stuff

--- Assets/Scripts/Vibe.cs
+++ Assets/Scripts/Vibe.cs
@@ -11,13 +11,7 @@ public class Vibe : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-<<<<<<< HEAD
-		Vector3 movement = new Vector3 (0.0f, 0.0f, -1);
-		transform.Translate (movement * Time.deltaTime);
-=======
-
 		Vector3 movement = new Vector3 (0.0f, 0.0f, -1);
 		transform.Translate (movement * Time.deltaTime * speed);
->>>>>>> 0b09a037f8709f2fe6e848e3cacbd19e1b99137f
 	}
 }
