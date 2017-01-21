989af287d2e055fe295439fff4bef4a5368f6848
Niko Arranz <nikoarranz@Nikos-MacBook-Air.local>
2017-01-21 17:19:13 -0500

    working spawn

--- Assets/Scripts/VibeSpawn.cs
+++ Assets/Scripts/VibeSpawn.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class VibeSpawn : MonoBehaviour, MasterClock.BeatListener {
 
 	public Rigidbody Vibe;
-	public Transform row;
+	public Transform row1;
+	public Transform row2;
+	public Transform row3;
+	public Transform row4;
+	public Transform row5;
+
 	private int counter = 0;
 
 	void Start () {
@@ -15,12 +20,20 @@ public class VibeSpawn : MonoBehaviour, MasterClock.BeatListener {
 		}
 	}
 
+	void Spawn (Transform x, Transform y) {
+		Instantiate (Vibe, x.position, y.rotation);
+	}
+
 	public void OnBeat (int bpm) {
 		if (counter % 4 == 0) {
-			Instantiate (Vibe, row.position, row.rotation);
+			Spawn (row1, row1);
+		} else if (counter % 6 == 0) {
+			Spawn (row4, row4);
+		} else if (counter % 6 == 0 && counter % 4 == 0) {
+			Spawn (row3, row3);
 		}
 		counter += 1;
 	}
 		
-	}
+}
 	
