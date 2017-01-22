0543e61d69937014b957eb0e5ecd44976920d6cf
Allan Pichardo <allanpichardo@Allans-MacBook-Pro.local>
2017-01-21 19:12:18 -0500

    making panel

--- Assets/Scripts/Beat/MasterClock.cs
+++ Assets/Scripts/Beat/MasterClock.cs
@@ -9,13 +9,14 @@ public class MasterClock : MonoBehaviour {
 	private Timer masterTimer;
 	private HashSet<BeatListener> listeners = new HashSet<BeatListener> ();
 
-	private int lastBpm = 60;
-	private bool lastEnabled = false;
-
 	public int bpm = 60;
 	public bool isEnabled = false;
 
 	private bool isOnset = false;
+
+	int lastBpm;
+
+	bool lastEnabled;
 
 	void Awake() {
 		masterTimer = new Timer(BeatUtils.BpmToMilliseconds(bpm));
@@ -30,20 +31,21 @@ public class MasterClock : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
-		if (lastEnabled != isEnabled) {
+		if (this.lastEnabled != this.isEnabled) {
 			Debug.Log ("Enabled: " + isEnabled);
-			masterTimer.Enabled = isEnabled;
-			lastEnabled = isEnabled;
+			masterTimer.Enabled = this.isEnabled;
+			this.lastEnabled = this.isEnabled;
 		}
-		if (lastBpm != bpm) {
+		if (this.lastBpm != this.bpm) {
 			Debug.Log ("BPM Change to: " + bpm);
 			masterTimer.Stop ();
 			masterTimer.Interval = BeatUtils.BpmToMilliseconds (bpm);
-			lastBpm = bpm;
+			this.lastBpm = bpm;
+			masterTimer.Enabled = isEnabled;
 		}
 
 		if (isOnset) {
---
+++ Assets/Scripts/Interface/ButtonPanel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPanel : MonoBehaviour, MasterClock.BeatListener {
+
+	public int steps = 8;
+	public Transform buttonPrefab;
+
+	private Transform[] buttons;
+	private int currentStep = 0;
+
+	void Awake() {
+		MasterClock clock = GameObject.FindObjectOfType<MasterClock> ();
+		if (clock != null) {
+			clock.AddListener (this);
+		}
+	}
+
+	private Vector3 GetPositionAtStep(int step) {
+		Vector3 panelPosition = transform.position;
+		float emptySpace = GetAvailableEmptySpace ();
+		float buttonWidth = ((RectTransform)buttonPrefab.transform).rect.width;
+		float x = emptySpace + step * (emptySpace + buttonWidth);
+
+		return new Vector3 (x, panelPosition.y, 0);
+	}
+
+	private float GetAvailableEmptySpace() {
+		float totalWidth = ((RectTransform)transform).rect.width;
+		float buttonWidth = ((RectTransform)buttonPrefab.transform).rect.width;
+
+		return (totalWidth - (buttonWidth * steps)) / (steps + 1);
+	}
+
+	// Use this for initialization
+	void Start () {
+		buttons = new Transform[steps];
+		for (int i = 0; i < steps; ++i) {
+			buttons [i] = Instantiate (buttonPrefab, GetPositionAtStep (i), Quaternion.identity);
+			GetButtonImage (i).SetButton (ButtonImage.Button.A);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private ButtonImage GetButtonImage(int index) {
+		return buttons [index].GetComponent<ButtonImage> ();
+	}
+
+	public void OnBeat(int bpm) {
+		int onStep = currentStep % steps;
+		int offStep = (currentStep - 1) % steps;
+
+		GetButtonImage (onStep).SetHighlight (true);
+		GetButtonImage (offStep).SetHighlight (false);
+
+		currentStep = (currentStep + 1) % steps;
+	}
+
+}
